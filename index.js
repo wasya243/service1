a1e3799d1793ff5f2e38d5c8cd5fc23697802d47
Danylo Halytskyi <rizorko@team-gcn.com>
2020-05-21 23:08:43 +0300

    Fixed env vars for easy Docker Compose integration

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, '.env') });
+// require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 const express = require('express');
 const axios = require('axios').default;
 const app = express();
 
-const {PORT, HOST, SERVICE_2_PORT, SERVICE_2_HOST, SERVICE_3_PORT, SERVICE_3_HOST} = process.env;
+const {PORT, HOST, SERVICE_2, SERVICE_3} = process.env;
 
 app.get('/sum', async (req, res) => {
     try {
         // const responseA = await axios.get(`${SERVICE_2_HOST}:${SERVICE_2_PORT}/a`);
         // const responseB = await axios.get(`${SERVICE_3_HOST}:${SERVICE_3_PORT}/b`);
 
-        const responseA = await axios.get(`172.17.0.2:${SERVICE_2_PORT}/a`);
-        const responseB = await axios.get(`172.17.0.3:${SERVICE_3_PORT}/b`);
+        const responseA = await axios.get(`${SERVICE_2}/a`);
+        const responseB = await axios.get(`${SERVICE_3}/b`);
 
         const sum = responseA.data + responseB.data;
 
